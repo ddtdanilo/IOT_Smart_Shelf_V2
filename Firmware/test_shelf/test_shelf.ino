c1342ce60ef8cbcde4dfffd1dd8f45657c471015
Carlos <cemoron93@gmail.com>
2019-02-14 15:37:37 -0600

    distance+load cell

--- Firmware/test_shelf/test_shelf.ino
+++ Firmware/test_shelf/test_shelf.ino
@@ -1,5 +1,8 @@
 #include <EEPROM.h>
 #include "HX711.h"
+#include "Adafruit_VL53L0X.h"
+
+//*********** Load Cell
 
 #define DT A1     //Load cell data pin (DT)
 #define SCK A0    //Load cell clock pin (SCK)
@@ -20,10 +23,49 @@ double ADC_Entrada[15];
 unsigned int contadorProducto = 0;
 bool producto = false;
 
+
+//********* Tray detection and traffic
+
+//I2C Address
+#define LOX1_ADDRESS 0x30
+#define LOX2_ADDRESS 0x31
+#define LOXt_ADDRESS 0x32
+
+//Set the pins to shutdown
+#define SHT_LOX1 7
+#define SHT_LOX2 6
+#define SHT_LOXt 5
+
+//Objects for the vl53l0x
+Adafruit_VL53L0X lox1 = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox2 = Adafruit_VL53L0X();
+Adafruit_VL53L0X loxt = Adafruit_VL53L0X();
+
+//Variables to hold the measurement
+VL53L0X_RangingMeasurementData_t measure1;
+VL53L0X_RangingMeasurementData_t measure2;
+VL53L0X_RangingMeasurementData_t measuret;
+
+unsigned int trayDetection = 0;
+unsigned int tray = 0;
+bool transit = false;
+
+unsigned int dist1 = 0;
+unsigned int dist2 = 0;
+unsigned int distt = 0;
+
+
+
 //SETUP
 void setup() {
 
   Serial.begin(115200);
+
+  //HAND DETECTION BAR
+  setOutputsLow();
+  setAddress();
+
+  //WEIGHT
   setCero();
 
 }
@@ -31,8 +73,13 @@ void setup() {
 //LOOP
 
 void loop() {
-
+  //CONTROL
   serialCommandReceive(); // for debugging purposes // 'e' for reset 0 and 'r'for reset eeprom and new fresh begin in the next restart
+
+  //HAND BAR
+  read_hand_bar();
+
+  //WEIGHT
   measureWeight();
   comprobacionParametros();
 }
@@ -207,7 +254,7 @@ double measureWeight() {
 }
 ///*******************************************
 void comprobacionParametros() {
-  bool graph = true;
+  bool graph = false;
   if (!graph)
   {
     Serial.println();
@@ -217,6 +264,22 @@ void comprobacionParametros() {
     Serial.print("pesoEEPROM: ");
     Serial.print(pesoEEPROM, 3);
     Serial.print(" | ");
+    Serial.print("Distancia 1: ");
+    Serial.print(dist1);
+    Serial.print(" | ");
+    Serial.print("Distancia 2: ");
+    Serial.print(dist2);
+    Serial.print(" | ");
+    Serial.print("Distancia t: ");
+    Serial.print(distt);
+    Serial.print(" | ");
+    Serial.print("Bandeja: ");
+    Serial.print(tray);
+    Serial.print(" | ");
+    Serial.print("Transito: ");
+    Serial.print(transit);
+    Serial.print(" | ");
+
   }
   else
   {
@@ -224,3 +287,147 @@ void comprobacionParametros() {
   }
 }
 ///************************************************************
+
+
+
+//********************* TRAFFIC and tray
+
+
+//FUNCTIONS
+
+void setOutputsLow() {
+  pinMode(SHT_LOX1, OUTPUT);
+  pinMode(SHT_LOX2, OUTPUT);
+  pinMode(SHT_LOXt, OUTPUT);
+
+  //Serial.println("Shutdown pins inited...");
+
+  digitalWrite(SHT_LOX1, LOW);
+  digitalWrite(SHT_LOX2, LOW);
+  digitalWrite(SHT_LOXt, LOW);
+
+  // Serial.println("All devices in reset mode...(pins are low)");
+}
+
+void setAddress() {
+  //All reset
+  digitalWrite(SHT_LOX1, LOW);
+  digitalWrite(SHT_LOX2, LOW);
+  digitalWrite(SHT_LOXt, LOW);
+
+  delay(10);
+
+  //All unreset
+  digitalWrite(SHT_LOX1, HIGH);
+  digitalWrite(SHT_LOX2, HIGH);
+  digitalWrite(SHT_LOXt, HIGH);
+
+  delay(10);
+
+  //Activating LOX1 and reseting the other devices
+  digitalWrite(SHT_LOX1, HIGH);
+  digitalWrite(SHT_LOX2, LOW);
+  digitalWrite(SHT_LOXt, LOW);
+
+  //Initing LOX1
+  if (!lox1.begin(LOX1_ADDRESS)) {
+    Serial.println(F("Failed to boot VL53L0X 1"));
+    while (1);
+  }
+  delay(10);
+
+  //Activating LOX2
+  digitalWrite(SHT_LOX2, HIGH);
+  delay(10);
+
+  //Initing LOX2
+  if (!lox2.begin(LOX2_ADDRESS)) {
+    Serial.println(F("Failed to boot VL53L0X 2"));
+    while (1);
+  }
+  delay(10);
+
+  //Activating LOXt
+  digitalWrite(SHT_LOXt, HIGH);
+  delay(10);
+
+  //Initing LOXt
+  if (!loxt.begin(LOXt_ADDRESS)) {
+    Serial.println(F("Failed to boot VL53L0X for traffic"));
+    while (1);
+  }
+
+  // Serial.println("All addresses set!");
+}
+
+void read_hand_bar() {
+
+
+  lox1.rangingTest(&measure1, false); // pass in 'true' to get debug data printout!
+  lox2.rangingTest(&measure2, false); // pass in 'true' to get debug data printout!
+  loxt.rangingTest(&measuret, false); // pass in 'true' to get debug data printout!
+
+  // print sensor one reading
+  /*
+    Serial.print("1: ");
+    if (measure1.RangeStatus != 4) {    // if not out of range
+    Serial.print(measure1.RangeMilliMeter);
+    } else {
+    Serial.print("Out of range");
+    }
+
+    Serial.println("  |  ");
+
+    // print sensor two reading
+    Serial.print("2: ");
+    if (measure2.RangeStatus != 4) {
+    Serial.print(measure2.RangeMilliMeter);
+    } else {
+    Serial.print("Out of range");
+    }
+
+    Serial.println("  |  ");
+
+    // print sensor traffic reading
+    Serial.print("t: ");
+    if (measuret.RangeStatus != 4) {
+    Serial.print(measuret.RangeMilliMeter);
+    } else {
+    Serial.print("Out of range");
+    }
+  */
+  // Serial.println();
+
+  dist1 = measure1.RangeMilliMeter;
+  dist2 = measure2.RangeMilliMeter;
+  distt = measuret.RangeMilliMeter;
+
+  if (dist1 < 410 || dist2 < 410) {
+    //Serial.println("****************BANDEJA 1");
+    if (trayDetection != 1 || trayDetection == 0) trayDetection = 1;
+    else if (trayDetection == 1) tray = 1;
+  }
+
+  else if (dist1 < 820 || dist2 < 820) {
+    //Serial.println("****************BANDEJA 2");
+    if (trayDetection != 2 || trayDetection == 0) trayDetection = 2;
+    else if (trayDetection == 2) tray = 2;
+  }
+
+  else if (dist1 < 1050 || dist2 < 1050) {
+    //Serial.println("****************BANDEJA 3");
+    if (trayDetection != 3 || trayDetection == 0) trayDetection = 3;
+    else if (trayDetection == 3) tray = 3;
+  }
+  else {
+    trayDetection = 0;
+    tray = 0;
+  }
+
+  if (distt < 500) {
+    transit = true;
+  }
+  else transit = false;
+
+  //delay(500);
+}
